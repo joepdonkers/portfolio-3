9ffee4ff203cdd5c322f41e671c1880872cf3ad8
joepdonkers <joepie.donkers@gmail.com>
2023-04-04 12:43:27 +0200

    added all tests?

--- TxHandler.java
+++ TxHandler.java
@@ -1,4 +1,4 @@
-import java.security.*;
+import java.util.ArrayList;
 
 public class TxHandler {
 
@@ -25,38 +25,36 @@ public class TxHandler {
 	 * and false otherwise.
 	 */
 
-	 public boolean isValidTx(Transaction tx) {
-		RSAKey address = new RSAKey(null, null);
+	public boolean isValidTx(Transaction tx) {
 		UTXOPool uniqueUtxos = new UTXOPool();
 		double previousTxOutSum = 0;
-		// IMPLEMENT THIS
-		// (1) FOR LOOP met zoeken in de UTSO pool
-		
+
 		for (int i = 0; i < tx.numInputs(); i++) {
 			Transaction.Input in = tx.getInput(i);
 			UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
 			Transaction.Output output = utxoPool.getTxOutput(utxo);
-	
+
 			// Check if output is in the current UTXO pool
 			if (!utxoPool.contains(utxo)) {
 				return false;
 			}
-	
+
 			// Check for double spending
 			if (uniqueUtxos.contains(utxo)) {
 				return false;
 			}
-	
+
 			uniqueUtxos.addUTXO(utxo, output);
 			previousTxOutSum += output.value;
-	
+
 			// Check signature validity
+			RSAKey address = output.address;
 			byte[] message = tx.getRawDataToSign(i);
-			if (address.verifySignature(message, in.signature)) {
+			if (!address.verifySignature(message, in.signature)) {
 				return false;
 			}
 		}
-	
+
 		double currentTxOutSum = 0;
 		for (Transaction.Output out : tx.getOutputs()) {
 			if (out.value < 0) {
@@ -64,31 +62,31 @@ public class TxHandler {
 			}
 			currentTxOutSum += out.value;
 		}
-	
+
 		// Check if output values are non-negative
 		if (previousTxOutSum < currentTxOutSum) {
 			return false;
 		}
-	
+
 		return true;
 	}
-		// (2) kijk of er wel een signature is meegegeven
-		// geverifieerd worden met de public, en de data die ondertekend is
-		// in de jar file zit een RSA class die je kan gebruiken om te verifiëren met
-		// .verify?
-		// (3) voorkomt dubbel spending binnen een transactieblok (check distinct!) alle
-		// inputs van het tx blok moeten een unieke index hebben
-		// (4) alle output values moeten groter zijn dan 0 anders kun je munten stelen
-		// van andere met negatieve waardes en bij jezelf dubbele waardes invullen als
-		// output
-		// lijkt op elevation of privelege of spoofing
-		// (5) voor de input moet je de output value pakken van de gelinkte input (dus
-		// de output van de vorige transactie)
-		// de som van alle input values moet groter zijn dan de som van alle output
-		// values
-		// de output mag kleiner zijn want scrooch vind het niet erg als er een beetje
-		// geld verloren gaat
-		// en de output mag niet groter zijn want dan geef je meer geld uit dan je hebt
+	// (2) kijk of er wel een signature is meegegeven
+	// geverifieerd worden met de public, en de data die ondertekend is
+	// in de jar file zit een RSA class die je kan gebruiken om te verifiëren met
+	// .verify?
+	// (3) voorkomt dubbel spending binnen een transactieblok (check distinct!) alle
+	// inputs van het tx blok moeten een unieke index hebben
+	// (4) alle output values moeten groter zijn dan 0 anders kun je munten stelen
+	// van andere met negatieve waardes en bij jezelf dubbele waardes invullen als
+	// output
+	// lijkt op elevation of privelege of spoofing
+	// (5) voor de input moet je de output value pakken van de gelinkte input (dus
+	// de output van de vorige transactie)
+	// de som van alle input values moet groter zijn dan de som van alle output
+	// values
+	// de output mag kleiner zijn want scrooch vind het niet erg als er een beetje
+	// geld verloren gaat
+	// en de output mag niet groter zijn want dan geef je meer geld uit dan je hebt
 
 	/*
 	 * Handles each epoch by receiving an unordered array of proposed
@@ -97,8 +95,28 @@ public class TxHandler {
 	 * and updating the current UTXO pool as appropriate.
 	 */
 	public Transaction[] handleTxs(Transaction[] possibleTxs) {
-		// IMPLEMENT THIS
-		return null;
+		ArrayList<Transaction> validTxs = new ArrayList<>();
+	
+		for (Transaction tx : possibleTxs) {
+			if (isValidTx(tx)) {
+				validTxs.add(tx);
+	
+				// Remove consumed UTXOs
+				for (Transaction.Input in : tx.getInputs()) {
+					UTXO utxo = new UTXO(in.prevTxHash, in.outputIndex);
+					utxoPool.removeUTXO(utxo);
+				}
+	
+				// Add created UTXOs
+				for (int i = 0; i < tx.numOutputs(); i++) {
+					Transaction.Output out = tx.getOutput(i);
+					UTXO utxo = new UTXO(tx.getHash(), i);
+					utxoPool.addUTXO(utxo, out);
+				}
+			}
+		}
+	
+		return validTxs.toArray(new Transaction[validTxs.size()]);
 	}
 
 }
